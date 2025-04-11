c0daa41d15fdd2ce3132048235d0d46ed1e53e7c
Subrahmanya Gaonkar <148525245+negativenagesh@users.noreply.github.com>
2025-04-11 04:56:37 +0000

    added nonce, cspSource as parameter for getewebviewcontent function

--- src/webviewContent.ts
+++ src/webviewContent.ts
@@ -1,11 +1,17 @@
 import * as vscode from 'vscode';
 
-export function getWebviewContent(stylesUri: vscode.Uri, scriptUri: vscode.Uri): string {
+export function getWebviewContent(
+    stylesUri: vscode.Uri, 
+    scriptUri: vscode.Uri,
+    nonce: string,
+    cspSource: string
+): string {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${cspSource} blob: data:; style-src ${cspSource}; script-src 'nonce-${nonce}'; media-src mediastream:;">
         <title>MoodLint</title>
         <link rel="stylesheet" href="${stylesUri}">
     </head>
@@ -81,7 +87,7 @@ export function getWebviewContent(stylesUri: vscode.Uri, scriptUri: vscode.Uri):
                 <p>MoodLint - Debugging aligned with your emotional state</p>
             </footer>
         </div>
-        <script src="${scriptUri}"></script>
+        <script nonce="${nonce}" src="${scriptUri}"></script>
     </body>
     </html>`;
 }
